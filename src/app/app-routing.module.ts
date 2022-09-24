08eb38103de65619796ecaf1aec2841b63444846
Jonna Hyypiä <jonna.hyypia@fi.experis.com>
2022-09-24 22:02:19 +0300

    Add login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { AuthGuard } from "./guards/auth.guard";
 import { LandingPage } from "./pages/landing/landing.page";
 import { PokemonCataloguePage } from "./pages/pokemon-catalogue/pokemon-catalogue.page";
 import { TrainerPage } from "./pages/trainer/trainer.page";
@@ -7,15 +8,22 @@ import { TrainerPage } from "./pages/trainer/trainer.page";
 const routes: Routes = [
     {
         path: "",
+        pathMatch: "full",
+        redirectTo: "/landing"
+    },
+    {
+        path: "landing",
         component: LandingPage
     },
     {
         path: "pokemon",
-        component: PokemonCataloguePage
+        component: PokemonCataloguePage,
+        canActivate: [ AuthGuard ]
     },
     {
         path: "trainer",
-        component: TrainerPage
+        component: TrainerPage,
+        canActivate: [ AuthGuard ]
     }
 ]
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
 import { LandingPage } from './pages/landing/landing.page';
 import { PokemonCataloguePage } from './pages/pokemon-catalogue/pokemon-catalogue.page';
 import { TrainerPage } from './pages/trainer/trainer.page';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPage,
     PokemonCataloguePage,
-    TrainerPage
+    TrainerPage,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
